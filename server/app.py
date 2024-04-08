935fff4e697939530c017dad9733ad7a3d340e6f
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-04-08 15:07:57 +0300

    Added the correct changes

--- server/app.py
+++ server/app.py
@@ -10,14 +10,40 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-
+    
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
 
 class Home(Resource):
-    pass
+    def get(self):
+        respond_dict={
+            "message": "Welcome to the Newsletter RESTful API"
+        }
+        response=make_response(respond_dict,200)
+        return response
+api.add_resource(Home,"/")
 
+class Newsletters(Resource):
+    def get(self):
+        response_dict_list=[n for n in Newsletter.query.all()]    
+        response=make_response(response_dict_list,200)
+        return response
+    
+    def post(self):
+        new_record=Newsletter(title=request.form["title"],body=request.form["body"])
+        db.session.add(new_record)
+        db.session.commit()
+        respond_dict=new_record.to_dict()
+        response=make_response(respond_dict,201)
+        return response
+api.add_resource(Newsletters,"/newsletters")
+class NewslettersById(Resource):
+    def get(self,id):
+        respond_dict=Newsletter.query.filter(Newsletter.id==id).first.to_dict()
+        response=make_response(respond_dict,200)
+        return response
+api.add_resource(NewslettersById,"/newsletters/<int:id>")
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
